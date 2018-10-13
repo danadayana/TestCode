af56ac045be2e651ee4576cb1f85d465dc108ba4
Dayana <Dayana@DESKTOP-NA0FV26>
2018-10-13 12:58:00 -0400

    2nd push

--- MagentoTest/src/test/java/MagentoTest.java
+++ MagentoTest/src/test/java/MagentoTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class MagentoTest {
@@ -12,7 +13,7 @@ public class MagentoTest {
 	public void postCredentials () throws Exception {
 		
 	String url = "http://www.magento.com";
-	WebDriver driver = new ChromeDriver();
+	WebDriver driver = new FirefoxDriver();
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	driver.get(url);
