892becfde65908c5900d0fa95ecd1b68b7897cb8
Dayana <Dayana@DESKTOP-NA0FV26>
2018-10-13 12:32:51 -0400

    1st push

---
+++ MagentoTest/src/test/java/MagentoTest.java
@@ -0,0 +1,38 @@
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MagentoTest {
+
+	@Test
+	public void postCredentials () throws Exception {
+		
+	String url = "http://www.magento.com";
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	driver.get(url);
+	
+	Main m = new Main(driver);
+	m.clickOnMyacc();
+	Thread.sleep(5000);
+	
+	
+	Login login = new Login(driver);
+	login.clickOnLogin();
+	login.typeEmail("sucheendra.abc@gmail.com");
+	login.typePass("Welcome123");
+	
+	
+	Logout logout = new Logout(driver);
+	logout.clickOnLogout();
+	
+	driver.quit();
+
+	}
+
+}
